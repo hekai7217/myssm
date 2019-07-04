b12986096d3b82e6477d6faac3065a6c32c3a711
hekai <441650416@qq.com>
2019-07-04 20:23:43 +0800

    shiro 和 web整合

---
+++ shiro_web_demo/src/com/lx/realms/ShiroRealm.java
@@ -0,0 +1,28 @@
+package com.lx.realms;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * create by hekai  2019-07-03
+ *
+ * @desc  自定义 realm
+ */
+public class ShiroRealm implements Realm {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return false;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
---
+++ shiro_web_demo/web/login.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hekai
+  Date: 2019-07-03
+  Time: 22:41
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+     login page
+  </body>
+</html>
