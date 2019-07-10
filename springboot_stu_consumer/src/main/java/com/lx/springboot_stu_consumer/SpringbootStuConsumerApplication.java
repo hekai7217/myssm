f7221f0a0dc853aefc7128dec38613792d6cc0bf
hekai <441650416@qq.com>
2019-07-10 16:24:41 +0800

    springboot  consumer

---
+++ springboot_stu_consumer/src/main/java/com/lx/springboot_stu_consumer/SpringbootStuConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.lx.springboot_stu_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStuConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStuConsumerApplication.class, args);
+    }
+
+}
