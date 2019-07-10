2eb93b422e0fb45093a16773ad6e094617280be2
hekai <441650416@qq.com>
2019-07-10 16:21:38 +0800

    springboot  priducer

---
+++ springboot_stu_privoder/src/main/java/com/lx/springboot_stu_privoder/SpringbootStuPrivoderApplication.java
@@ -0,0 +1,13 @@
+package com.lx.springboot_stu_privoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStuPrivoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStuPrivoderApplication.class, args);
+    }
+
+}
---
+++ springboot_stu_privoder/src/test/java/com/lx/springboot_stu_privoder/SpringbootStuPrivoderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lx.springboot_stu_privoder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootStuPrivoderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
